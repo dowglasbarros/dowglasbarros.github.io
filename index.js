b879823a4e52eaef1a5b74fa2614f4863eebdf4c
Dowglas <dowglas_rodrigo@hotmail.com>
2019-01-13 12:09:47 -0200

    Updating code activity and main card

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-window.onload = function() {
-    var c = document.getElementById("binary-code");
-        c.width = window.innerWidth;
-        c.height = window.innerHeight;
+window.onload = function () {
+  let c = document.getElementById("binary-code");
+  c.width = window.innerWidth;
+  c.height = window.innerHeight;
+  let ctx = c.getContext("2d");
+  const matrix = "0100010001101111011101110110" + "0111011011000110000101110011".split(1);
+  const font_size = 9;
+  const columns = c.width / font_size;
+  let drops = [];
 
-    var ctx = c.getContext("2d");
-    var matrix = "0100010001101111011101110110" + "0111011011000110000101110011".split(1);
-    var font_size = 9;
-    var columns = c.width / font_size;
-    var drops = [];
+  for (let x = 0; x < columns; x++) {
+    drops[x] = x - 130;
+  }
 
-    for (var x = 0; x < columns; x++) {
-        drops[x] = x - 130;
+  function draw() {
+    ctx.fillStyle = "rgba(0, 0, 0, .05)";
+    ctx.fillRect(0, 0, c.width, c.height);
+    ctx.fillStyle = "#00DD00";
+    ctx.font = font_size;
+
+    for (let i = 0; i < drops.length; i++) {
+      let text = matrix[Math.floor(Math.random() * matrix.length)];
+      ctx.fillText(text, i * font_size, drops[i] * font_size);
+
+      if (drops[i] * font_size > c.height && Math.random() > 0.975) {
+        drops[i] = 0;
+      }
+
+      drops[i]++;
     }
+  }
 
-    function draw() {
-        ctx.fillStyle = "rgba(0, 0, 0, .05)";
-        ctx.fillRect(0, 0, c.width, c.height);
-        ctx.fillStyle = "#00DD00";
-        ctx.font = font_size;
-
-        for (var i = 0; i < drops.length; i++) {
-            var text = matrix[Math.floor(Math.random() * matrix.length)];
-            ctx.fillText(text, i * font_size, drops[i] * font_size);
-
-            if (drops[i] * font_size > c.height && Math.random() > 0.975) {
-                drops[i] = 0;
-            }
-
-            drops[i]++;
-        }
-    }
-
-    setInterval(draw, 40);
+  setInterval(draw, 40);
 };
